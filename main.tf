fc1c17f3b19b929e929b1a761647976dd1acd952
Ricardo Oliveira <17067463+gitrgoliveira@users.noreply.github.com>
2021-06-03 15:18:22 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
     organization = "infrastructure-pipelines-workshop"
 
     workspaces {
-      name = "john-d-consul"
+      name = "ricardo-o-consul"
     }
   }
   required_providers {
